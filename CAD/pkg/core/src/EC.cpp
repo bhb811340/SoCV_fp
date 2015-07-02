b8c42b3ec0096b87f6b73e96165f194c05836088
CYW <w781204@gmail.com>
2015-07-02 22:06:39 +0800

    add EC.cpp xor 	modified:   pkg/core/src/EC.cpp

--- CAD/pkg/core/src/EC.cpp
+++ CAD/pkg/core/src/EC.cpp
@@ -96,7 +96,36 @@ void EC::ckt2sat(Circuit ckt, Sat s){
 		    s.addClause(clause);
 		    clause.resetVariable();
 		}
-	}
+	    else if(gateType == "xor"){
+            //XOR (i1'+i2'+i3'+...io')(i1'+i2+i3+...+io)(i1+i2'+...+io)(i1+i2+...+io')
+            //(i1'+i2+i3+...+io)(i1+i2'+...+io)
+            for(unsigned j = 0; j < circuit().gate(i).numInWire(); ++j){
+                clause.addVariable((-1)*sat().wireIdToVariableId(circuit().gate(i).inWire(j)));
+                for(unsigned k = 0; k < circuit().gate(i).numInWire(); ++k){
+                    if(k != j){
+                        clause.addVariable(sat().wireIdToVariableId(circuit().gate(i).inWire(j)));
+                    }
+                }
+                clause.addVariable(sat().wireIdToVariableId(circuit().gate(i).outWire()));
+                sat().addClause(clause);
+                clause.resetVariable();
+            }
+            //(i1+i2+...+io')
+            for(unsigned j = 0 ; j < circuit().gate(i).numInWire() ; ++j){
+                clause.addVariable(sat().wireIdToVariableId(circuit().gate(i).inWire(j)));
+            }
+            clause.addVariable((-1)*sat().wireIdToVariableId(circuit().gate(i).outWire()));
+            sat().addClause(clause);
+            clause.resetVariable();
+            //(i1'+i2'+i3'+...io')
+            for(unsigned j = 0 ; j < circuit().gate(i).numInWire() ; ++j){
+                clause.addVariable((-1)*sat().wireIdToVariableId(circuit().gate(i).inWire(j)));
+            }
+            clause.addVariable((-1)*sat().wireIdToVariableId(circuit().gate(i).outWire()));
+            sat().addClause(clause);
+            clause.resetVariable();
+        }
+    }
 }
 
 bool EC::solveSat(Sat s) {
